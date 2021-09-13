4edbb772c009c6e2ac2af0daeefa33a0c615b24e
is-hoku <this@is-hoku.info>
2021-09-13 22:27:24 +0900

    add transactions

--- server/controllers/shop.go
+++ server/controllers/shop.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/youngeek-0410/mottake/server/db"
 	"github.com/youngeek-0410/mottake/server/models"
+	"gorm.io/gorm"
 )
 
 type ShopController struct{}
@@ -14,11 +15,14 @@ type ShopController struct{}
 func (i ShopController) Get(c *gin.Context) {
 	var shop models.Shop
 	uid := c.Param("uid")
-	if err := db.DB.Where("UID=?", uid).First(&shop).Error; err != nil {
-		c.JSON(http.StatusNotFound, nil)
-		log.Println(err)
-		return
-	}
+	db.DB.Transaction(func(tx *gorm.DB) error {
+		if err := db.DB.Where("UID=?", uid).First(&shop).Error; err != nil {
+			c.JSON(http.StatusNotFound, nil)
+			log.Println(err)
+			return err
+		}
+		return nil
+	})
 	c.JSON(http.StatusOK, shop)
 }
 
@@ -29,7 +33,7 @@ func (i ShopController) Post(c *gin.Context) {
 		log.Println(err)
 		return
 	}
-	shop.UID = getUID(c)
+	shop.UID = getUID(c) // ボディのUIDにトークンのUIDを保存
 	coordinate, err := AddressToCoordinate(shop.Address)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, nil)
@@ -38,11 +42,15 @@ func (i ShopController) Post(c *gin.Context) {
 	}
 	shop.Latitude = coordinate.Latitude
 	shop.Longitude = coordinate.Longitude
-	if err := db.DB.Create(&shop).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, nil)
-		log.Println(err)
-		return
-	}
+
+	db.DB.Transaction(func(tx *gorm.DB) error {
+		if err := db.DB.Create(&shop).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, nil)
+			log.Println(err)
+			return err
+		}
+		return nil
+	})
 	c.JSON(http.StatusOK, shop)
 }
 
@@ -68,11 +76,15 @@ func (i ShopController) Patch(c *gin.Context) {
 	}
 	shop.Latitude = coordinate.Longitude
 	shop.Longitude = coordinate.Longitude
-	if err := db.DB.Updates(&shop).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, nil)
-		log.Println(err)
-		return
-	}
+
+	db.DB.Transaction(func(tx *gorm.DB) error {
+		if err := db.DB.Updates(&shop).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, nil)
+			log.Println(err)
+			return err
+		}
+		return nil
+	})
 	c.JSON(http.StatusOK, shop)
 }
 
@@ -84,11 +96,15 @@ func (i ShopController) Delete(c *gin.Context) {
 		log.Println(err)
 		return
 	}
-	if err := db.DB.Delete(&shop).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, nil)
-		log.Println(err)
-		log.Println("uid=", uid)
-		return
-	}
+
+	db.DB.Transaction(func(tx *gorm.DB) error {
+		if err := db.DB.Delete(&shop).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, nil)
+			log.Println(err)
+			log.Println("uid=", uid)
+			return err
+		}
+		return nil
+	})
 	c.JSON(http.StatusOK, shop)
 }
