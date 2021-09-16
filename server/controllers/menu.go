110928759de5000e1c9987f9eb7cd7e42678ef45
is-hoku <this@is-hoku.info>
2021-09-17 00:07:37 +0900

    fix error messages

--- server/controllers/menu.go
+++ server/controllers/menu.go
@@ -6,18 +6,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
-	"github.com/pkg/errors"
 	"github.com/youngeek-0410/mottake/server/models"
-)
-
-var (
-	ErrInvalidJSONRequest = errors.New("invalid json request")
-	ErrCouldNotCreateMenu = errors.New("could not create menu")
-	ErrCouldNotGetMenus   = errors.New("could not get menus")
-	ErrCouldNotGetMenu    = errors.New("could not get menu")
-	ErrInvalidMenuID      = errors.New("invalid menu_id")
-	ErrCouldNotDeleteMenu = errors.New("could not delete menu")
-	ErrCouldNotUpdateMenu = errors.New("could not update menu")
 )
 
 type MenuController struct{}
@@ -28,13 +17,13 @@ func (m MenuController) Create(c *gin.Context) {
 	var menu models.Menu
 	uid := getUID(c)
 	if err := c.ShouldBindJSON(&menu); err != nil {
-		_ = c.Error(ErrInvalidJSONRequest).SetType(gin.ErrorTypePublic)
+		_ = c.Error(errInvalidJSONRequest).SetType(gin.ErrorTypePublic)
 		return
 	}
 
 	menuID, err := menuModel.Create(uid, menu)
 	if err != nil {
-		_ = c.Error(ErrCouldNotCreateMenu).SetType(gin.ErrorTypePublic)
+		_ = c.Error(errCouldNotCreateMenu).SetType(gin.ErrorTypePublic)
 		return
 	}
 	menu, _ = menuModel.GetOneByID(uid, menuID)
@@ -45,7 +34,7 @@ func (m MenuController) All(c *gin.Context) {
 	shopUID := c.Param("uid")
 	menus, err := menuModel.All(shopUID)
 	if err != nil {
-		_ = c.Error(ErrCouldNotGetMenus).SetType(gin.ErrorTypePublic)
+		_ = c.Error(errCouldNotGetMenus).SetType(gin.ErrorTypePublic)
 		return
 	}
 	c.JSON(http.StatusOK, menus)
@@ -56,12 +45,12 @@ func (m MenuController) One(c *gin.Context) {
 	shopUID := c.Param("uid")
 	menuID, err := strconv.Atoi(s)
 	if err != nil {
-		_ = c.Error(ErrInvalidMenuID).SetType(gin.ErrorTypePublic)
+		_ = c.Error(errInvalidMenuID).SetType(gin.ErrorTypePublic)
 		return
 	}
 	menu, err := menuModel.GetOneByID(shopUID, menuID)
 	if err != nil {
-		_ = c.Error(ErrCouldNotGetMenu).SetType(gin.ErrorTypePublic)
+		_ = c.Error(errCouldNotGetMenu).SetType(gin.ErrorTypePublic)
 		return
 	}
 	c.JSON(http.StatusOK, menu)
@@ -72,12 +61,12 @@ func (m MenuController) Delete(c *gin.Context) {
 	s := c.Param("menuID")
 	menuID, err := strconv.Atoi(s)
 	if err != nil {
-		_ = c.Error(ErrInvalidMenuID).SetType(gin.ErrorTypePublic)
+		_ = c.Error(errInvalidMenuID).SetType(gin.ErrorTypePublic)
 		return
 	}
 	err = menuModel.Delete(uid, menuID)
 	if err != nil {
-		_ = c.Error(ErrCouldNotDeleteMenu).SetType(gin.ErrorTypePublic)
+		_ = c.Error(errCouldNotDeleteMenu).SetType(gin.ErrorTypePublic)
 		return
 	}
 	c.JSON(http.StatusNoContent, nil)
@@ -90,17 +79,17 @@ func (m MenuController) Update(c *gin.Context) {
 	s := c.Param("menuID")
 	menuID, err := strconv.Atoi(s)
 	if err != nil {
-		_ = c.Error(ErrInvalidMenuID).SetType(gin.ErrorTypePublic)
+		_ = c.Error(errInvalidMenuID).SetType(gin.ErrorTypePublic)
 		return
 	}
 	err = json.NewDecoder(c.Request.Body).Decode(&menu)
 	if err != nil {
-		_ = c.Error(ErrInvalidJSONRequest).SetType(gin.ErrorTypePublic)
+		_ = c.Error(errInvalidJSONRequest).SetType(gin.ErrorTypePublic)
 		return
 	}
 	err = menuModel.Update(uid, menuID, menu)
 	if err != nil {
-		_ = c.Error(ErrCouldNotUpdateMenu).SetType(gin.ErrorTypePublic)
+		_ = c.Error(errCouldNotUpdateMenu).SetType(gin.ErrorTypePublic)
 		return
 	}
 	c.JSON(http.StatusNoContent, nil)
--- server/controllers/shop.go
+++ server/controllers/shop.go
@@ -7,18 +7,6 @@ import (
 	"github.com/youngeek-0410/mottake/server/models"
 )
 
-var (
-	ErrNotFound           = "Not Found"
-	ErrCouldNotCreateShop = "Could Not Create Shop"
-	ErrInvalidName        = "Invalid character or length (1~30)"
-	ErrInvalidDescription = "Invalid character of length (1~255)"
-	ErrInvalidAddress     = "Invalid Address. Please enter a proper address in Japanese"
-	ErrCouldNotUpdateShop = "Could Not Update Shop"
-	ErrInvalidJSON        = "Bad Request (invalid json)"
-	ErrNotAuthorized      = "Not Authorized"
-	ErrCouldNotDeleteShop = "Could Not Delete Shop"
-)
-
 type ShopController struct{}
 
 var shopModel = new(models.ShopModel)
@@ -27,7 +15,7 @@ func (i ShopController) GetByID(c *gin.Context) {
 	uid := c.Param("uid")
 	shop, err := shopModel.GetByID(uid)
 	if err != nil {
-		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusNotFound, ErrNotFound})
+		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusNotFound, errNotFound})
 		return
 	}
 	c.JSON(http.StatusOK, shop)
@@ -36,23 +24,23 @@ func (i ShopController) GetByID(c *gin.Context) {
 func (i ShopController) Create(c *gin.Context) {
 	var shop models.Shop
 	if err := c.BindJSON(&shop); err != nil {
-		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusBadRequest, ErrInvalidJSON})
+		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusBadRequest, errInvalidJSONRequest})
 		return
 	}
 
 	// Nameのバリデーション
 	if match := NameRegexp.MatchString(shop.Name); match == false {
-		c.Error(nil).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusBadRequest, ErrInvalidName})
+		c.Error(nil).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusBadRequest, errInvalidName})
 		return
 	}
 	// Descriptionのバリデーション
 	if match := DescriptionRegexp.MatchString(shop.Description); match == false {
-		c.Error(nil).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusBadRequest, ErrInvalidDescription})
+		c.Error(nil).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusBadRequest, errInvalidDescription})
 		return
 	}
 	coordinate, err := AddressToCoordinate(shop.Address)
 	if err != nil {
-		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusBadRequest, ErrInvalidAddress})
+		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusBadRequest, errInvalidAddress})
 		return
 	}
 	uid := getUID(c)
@@ -61,7 +49,7 @@ func (i ShopController) Create(c *gin.Context) {
 
 	returnedShop, err := shopModel.Create(shop, uid, latitude, longitude)
 	if err != nil {
-		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusInternalServerError, ErrCouldNotCreateShop})
+		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusInternalServerError, errCouldNotCreateShop})
 		return
 	}
 	c.JSON(http.StatusOK, returnedShop)
@@ -71,25 +59,25 @@ func (i ShopController) Update(c *gin.Context) {
 	uid := getUID(c)
 	var shop models.Shop
 	if err := c.BindJSON(&shop); err != nil {
-		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusBadRequest, ErrInvalidJSON})
+		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusBadRequest, errInvalidJSONRequest})
 		return
 	}
 
 	// Nameのバリデーション
 	// Nameが空の時はバリデーションを回避（ゼロ値はUpdateされないため）
 	if shop.Name != "" && NameRegexp.MatchString(shop.Name) == false {
-		c.Error(nil).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusBadRequest, ErrInvalidName})
+		c.Error(nil).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusBadRequest, errInvalidName})
 		return
 	}
 	// Descriptionのバリデーション
 	// Nameが空の時はバリデーションを回避（ゼロ値はUpdateされないため）
 	if shop.Name != "" && DescriptionRegexp.MatchString(shop.Description) == false {
-		c.Error(nil).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusBadRequest, ErrInvalidDescription})
+		c.Error(nil).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusBadRequest, errInvalidDescription})
 		return
 	}
 	coordinate, err := AddressToCoordinate(shop.Address)
 	if err != nil {
-		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusBadRequest, ErrInvalidAddress})
+		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusBadRequest, errInvalidAddress})
 		return
 	}
 	latitude := coordinate.Latitude
@@ -97,7 +85,7 @@ func (i ShopController) Update(c *gin.Context) {
 
 	returnedshop, err := shopModel.Update(shop, uid, latitude, longitude)
 	if err != nil {
-		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusInternalServerError, ErrCouldNotUpdateShop})
+		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusInternalServerError, errCouldNotUpdateShop})
 		return
 	}
 	c.JSON(http.StatusOK, returnedshop)
@@ -108,7 +96,7 @@ func (i ShopController) Delete(c *gin.Context) {
 	var shop models.Shop
 	shop, err := shopModel.Delete(shop, uid)
 	if err != nil {
-		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusInternalServerError, ErrCouldNotDeleteShop})
+		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusInternalServerError, errCouldNotDeleteShop})
 		return
 	}
 	c.JSON(http.StatusOK, shop)
