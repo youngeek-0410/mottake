a99b9ff56051080a624c405bcf55a67be6144ecd
is-hoku <this@is-hoku.info>
2021-09-20 22:03:05 +0900

    fix gorm #41

--- server/models/purchace.go
+++ server/models/purchace.go
@@ -1,10 +1,9 @@
 package models
 
 type Purchase struct {
-	ID        int      `json:"id" gorm:"primaryKey"`
-	Menu      *Menu    `json:"menu,omitempty"`
-	MenuID    int      `json:"menu_id"`
-	Receipt   *Receipt `json:"receipt,omitempty"`
-	ReceiptID int      `json:"receipt_id"`
-	Number    int      `json:"number"`
+	ID        int  `json:"id" gorm:"primaryKey"`
+	Menu      Menu `json:"menu,omitempty" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	MenuID    int  `json:"menu_id"`
+	ReceiptID int  `json:"receipt_id"`
+	Number    int  `json:"number"`
 }
